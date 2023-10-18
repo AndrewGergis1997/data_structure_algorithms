dc50ebcb92f64641834a9ae927103a828f1c5afa
27 <andrew.gergis@tuni.fi>
2023-10-19 00:09:41 +0300

    stl algorith 7 done

--- wk07/stl/stl7.cpp
+++ wk07/stl/stl7.cpp
@@ -16,5 +16,19 @@ using namespace std;
  */
 int findMedian(std::vector<int>& v)
 {
+    int size = v.size();
+    std::sort(v.begin(), v.end());
+    if (size ==0){
+        return NOT_FOUND;
+    }
+    else if ((v.size() % 2) == 0)
+    {
+     return (v[size/2-1] + v[size/2]) / 2;
+    }
+    else
+    {
+    return v[size/2];
+    }
+
 }
 
